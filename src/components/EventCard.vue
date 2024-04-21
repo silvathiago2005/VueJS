343dcdd4fe3b21714c286d08604ce02c00a7e886
Thiago Lopes da Silva <silvathiago2005@gmail.com>
2024-04-21 12:50:37 -0300

    second deploy
    second deploy

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: Object,
+                default: () => {} 
+            },
+        },
+        data() {
+            return {
+                key: value
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
